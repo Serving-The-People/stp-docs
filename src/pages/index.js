3cf7ebadfa80ee1318a5e069c90a027d6698cf12
Sam Grund <samgrund@Sams-MBP.fios-router.home>
2022-05-19 19:20:32 -0400

    chore: fix build

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,6 @@ import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "./index.module.css";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
-import { getUrlParam } from "../lib/getUrlParam";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
